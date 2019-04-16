dc1c0835b5035dff1ae9273412dcfdc7f8c74e43
Mikhail Yermolayev <ermolaev@targetprocess.com>
2019-04-16 11:52:44 +0300

    Updates

--- src/hooks/storeConsumers.js
+++
@@ -1,7 +0,0 @@
-import { useStoreConsumer } from "../../../src";
-
-
-export const storeConsumers = {
-    useCounterStore: () => useStoreConsumer(`counter`),
-    useMessageStore: () => useStoreConsumer(`message`),
-};
